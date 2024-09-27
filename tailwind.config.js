d23017907464d00fcd3930dbf3dacfebe637c07e
Vanessa Ezeh <166334915+vanessaezeh8@users.noreply.github.com>
2024-09-26 20:22:52 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,7 +50,7 @@ module.exports = {
   				'5': 'hsl(var(--chart-5))'
   			}
   		},
-  		borderRadius: {
+  		border-radius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
